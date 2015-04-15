d54da53504bc6f256b8601a9bdbeecdc6833534a
Daniel Krzywicki <eleaar@gmail.com>
2015-04-15 14:33:10 +0200

    cleanup local search helpers

--- src/main/scala/org/krzywicki/golomb/GolombLocalSearch.scala
+++ src/main/scala/org/krzywicki/golomb/GolombLocalSearch.scala
@@ -1,11 +1,17 @@
 package org.krzywicki.golomb
 
-import org.krzywicki.localsearch.LocalSearchStrategy
+import org.krzywicki.golomb.GolombLocalSearch.MarkChange
+import org.krzywicki.localsearch.{LocalSearchHelper, LocalSearchStrategy}
 import pl.edu.agh.scalamas.genetic.GeneticEvaluator
 
-/**
- * Created by Daniel on 2015-04-15.
- */
+import scala.collection.mutable.ArrayBuffer
+
+object GolombLocalSearch {
+
+  case class MarkChange(index: Int, mark: Int)
+
+}
+
 trait GolombLocalSearch extends GeneticEvaluator[GolombOps] {
 
   def localSearchStrategy: LocalSearchStrategy[GolombOps]
@@ -16,5 +22,49 @@ trait GolombLocalSearch extends GeneticEvaluator[GolombOps] {
     val baseEvaluation = super.evaluate(s)
 
     localSearchStrategy.search(baseEvaluation, helper)
+  }
+}
+
+class GolombLocalSearchHelper(marks: Array[Int]) extends LocalSearchHelper[MarkChange, GolombOps] {
+
+  lazy val distances = GolombEvaluator.distancesMap(marks)
+
+  override def possibleChanges = {
+    val size = marks.length
+    val changes = ArrayBuffer[MarkChange]()
+
+    // Changes to '0' mark moves the ruler
+    // Changing from 1st to (n - 1)th mark
+    var i = 1
+    while (i < size - 1) {
+      var j = marks(i - 1) + 1
+      val maxIdx = marks(i + 1) - 1
+      while (j <= maxIdx) {
+        changes += MarkChange(i, j)
+        j += 1
+      }
+      i += 1
+    }
+    // Changing last mark
+    i = marks(size - 2) + 1
+    val maxIdx = marks(size - 1) - 1
+    while (i <= maxIdx) {
+      changes += MarkChange(size - 1, i)
+      i += 1
+    }
+
+    changes.toStream
+  }
+
+  override def evaluateChange(change: MarkChange) = {
+    val newDistances = GolombEvaluator.distancesMapAfterFlip(marks, distances, change.index, change.mark)
+    val newViolations = GolombEvaluator.distanceViolations(newDistances)
+    GolombEvaluator.evaluateWithViolations(marks, newViolations)
+  }
+
+  override def applyChange(change: MarkChange) = {
+    val newMarks = marks.clone()
+    newMarks(change.index) = change.mark
+    new GolombLocalSearchHelper(newMarks)
   }
 }
--- src/main/scala/org/krzywicki/labs/LabsLocalSearch.scala
+++ src/main/scala/org/krzywicki/labs/LabsLocalSearch.scala
@@ -1,16 +1,35 @@
 package org.krzywicki.labs
 
-import org.krzywicki.localsearch.LocalSearchStrategy
+import org.krzywicki.localsearch.{LocalSearchHelper, LocalSearchStrategy}
 import pl.edu.agh.scalamas.genetic.GeneticEvaluator
+import org.krzywicki.labs.LabsLocalSearch.BitFlip
 
-/**
- * Created by Daniel on 2015-04-14.
- */
+object LabsLocalSearch {
+
+  case class BitFlip(bitToFlip: Int)
+
+}
+
 trait LabsLocalSearch extends GeneticEvaluator[LabsOps] {
 
   def localSearchStrategy: LocalSearchStrategy[LabsOps]
 
   abstract override def evaluate(s: LabsOps#Solution) = {
     localSearchStrategy.search(super.evaluate(s), new LabsLocalSearchHelper(s))
+  }
+}
+
+class LabsLocalSearchHelper(solution: LabsOps#Solution) extends LocalSearchHelper[BitFlip, LabsOps] {
+
+  lazy val flipper = OneBitFastFlipper(solution)
+
+  override def possibleChanges = Stream.tabulate(solution.length)(BitFlip(_))
+
+  override def evaluateChange(change: BitFlip) = flipper.energyWithFlipped(change.bitToFlip)
+
+  override def applyChange(change: BitFlip) = {
+    val copy = solution.clone()
+    copy(change.bitToFlip) = !copy(change.bitToFlip)
+    new LabsLocalSearchHelper(copy)
   }
 }
